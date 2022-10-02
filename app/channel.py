46a9e07f705bf1661e7486d4ea04089fe653d211
Sujan Sivaloganathan <sujan.sivaloganathan@uwaterloo.ca>
2022-10-01 21:46:02 -0400

    use batch update and insert in /refresh

--- app/channel.py
+++ app/channel.py
@@ -5,12 +5,12 @@ from flask import (
 from app.db import get_db
 from app.utils.api import fetch_channel_data, fetch_video_data
 from app.utils.db import (
-    add_interactions_to_video_interactions_table,
     add_channel_to_channels_table,
+    batch_add_interactions_to_video_interactions_table,
     get_pk_from_channels_table,
-    get_pks_from_videos_table_by_channel_id,
+    get_video_from_videos_table_by_channel_id,
     update_channel_in_channels_table,
-    update_most_recent_interaction_in_video_interactions_table)
+    update_most_recent_interactions_in_video_interactions_table)
 
 bp = Blueprint('channel', __name__, url_prefix='/channel')
 
@@ -57,23 +57,31 @@ def refresh():
         db.rollback()
         return 'Channel data could not be refreshed in the database.', 500
 
-    associated_videos = get_pks_from_videos_table_by_channel_id(db, channel_pk)
+    associated_videos = get_video_from_videos_table_by_channel_id(
+        db, channel_pk)
+
+    existing_video_interactions_to_update = []
+    new_video_interactions_to_insert = []
 
     for video in associated_videos:
         video_pk = video['id']
         video_id = video['video_id']
 
-        try:
-            video_data = fetch_video_data(video_id)
+        existing_video_interactions_to_update.append((video_pk,))
 
-            # update the existing row by setting latest to 0, then insert a new row with fresh data
-            update_most_recent_interaction_in_video_interactions_table(
-                db, video_pk)
-            add_interactions_to_video_interactions_table(
-                db, video_pk, video_data)
-        except:
-            db.rollback()
-            return 'Channel data could not be refreshed in the database.', 500
+        video_data = fetch_video_data(video_id)
+        new_video_interactions_to_insert.append(
+            (video_pk, video_data['view_count'], video_data['like_count'], video_data['comment_count'], 1,))
+
+    try:
+        # update the existing row by setting latest to 0, then insert a new row with fresh data
+        update_most_recent_interactions_in_video_interactions_table(
+            db, existing_video_interactions_to_update)
+        batch_add_interactions_to_video_interactions_table(
+            db, new_video_interactions_to_insert)
+    except:
+        db.rollback()
+        return 'Channel data could not be refreshed in the database.', 500
 
     db.commit()
     return f'The data has been refreshed for channel {channel_id} and all of its associated videos'
